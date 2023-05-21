a4e601514742821b829510acaaea63783cc7ed21
Dilshan7542 <dilshan.maduranga7542@gmail.com>
2023-05-21 22:11:49 +0530

    add loading mode

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -28,3 +28,42 @@ $("#serviceSection>section>div").on("mouseenter",function () {
    /* $(this).addClass("hvr-pulse-grow");*/
 });
 
+var next=0;
+$("#page-4 .downSection > section > aside >:nth-child(1)").click(function (){
+    next>0 ? next--:0;
+    hideAllServices();
+    switch (next) {
+        case 0:
+            $(`#page-4 .downSection #serviceSection section >:nth-child(1)`).css({"display":"flex","transition":"all 4s"});
+            $(`#page-4 .downSection #serviceSection section >:nth-child(2)`).css("display","flex");
+            break;
+        case 1:
+            $(`#page-4 .downSection #serviceSection section > :nth-child(3)`).css("display","flex");
+            $(`#page-4 .downSection #serviceSection section > :nth-child(4)`).css("display","flex");
+            break;
+    }
+});
+$("#page-4 .downSection > section > aside >:nth-child(2)").click(function (){
+    next<2 ? next++:0;
+    hideAllServices();
+    switch (next) {
+        case 1:
+            $(`#page-4 .downSection #serviceSection section >:nth-child(3)`).css("display","flex");
+            $(`#page-4 .downSection #serviceSection section >:nth-child(4)`).css("display","flex");
+            break;
+        case 2:
+            $(`#page-4 .downSection #serviceSection section >:nth-child(5)`).css("display","flex");
+            $(`#page-4 .downSection #serviceSection section >:nth-child(6)`).css("display","flex");
+            break;
+    }
+});
+
+function hideAllServices(){
+    $(`#page-4 .downSection #serviceSection section > div`).hide();
+}
+$(window).resize(function (){
+        if(window.innerWidth>450){
+            $(`#page-4 .downSection #serviceSection section > div`).show();
+
+        }
+});
